83891f50e54f5c0cf16fda92c294ac4c95853c13
muslax <muslax@gmail.com>
2022-02-01 10:21:53 +0700

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,12 +1,14 @@
 export const extract = (data: any, field: string) => {
   const result: any = {};
   for (const res of data) {
-    const key = res[field] || "_EMPTY_";
+    const isEmpty = ('' + res[field]).trim().length == 0;
+    const key = isEmpty ? "_EMPTY_" : res[field];
     if (result[key] == undefined) {
       result[key] = 1;
     } else {
       const n = result[key];
-      result[res[field]] = n + 1;
+      // result[res[field]] = n + 1;
+      result[key] = n + 1;
     }
   }
 
